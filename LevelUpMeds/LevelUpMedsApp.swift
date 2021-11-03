0f1bb61ca5f985d73b29194237e745de7b2a82c5
Brevin Blalock <30970021+BrevinB@users.noreply.github.com>
2021-11-03 19:37:37 -0400

    Initial Commit

---
+++ LevelUpMeds/LevelUpMedsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LevelUpMedsApp.swift
+//  LevelUpMeds
+//
+//  Created by Brevin Blalock on 11/3/21.
+//
+
+import SwiftUI
+
+@main
+struct LevelUpMedsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
