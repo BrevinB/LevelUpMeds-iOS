804dbebc9c146a903144760415321b4b31ed742c
Brevin Blalock <bblalock313@gmail.com>
2022-04-19 14:16:39 -0400

    Commit for current beta version 0.1.1 build #6

--- LevelUpMeds/LevelUpMedsApp.swift
+++ LevelUpMeds/LevelUpMedsApp.swift
@@ -13,32 +13,67 @@ struct LevelUpMedsApp: App {
     
     @StateObject var sessionService = SessionServiceImpl()
     @StateObject var signUp = RegistrationViewModelImpl()
+    @AppStorage("needsAppOnBoarding") private var needsAppOnboarding: Bool = true
+    @AppStorage("notificationScheduleSet") private var notificationScheduleSet: Bool = false
+    let currentNotification = UNUserNotificationCenter.current()
 
     
     init() {
         FirebaseApp.configure()
+        //FirebaseConfiguration.shared.setLoggerLevel(.min)
     }
    
     var body: some Scene {
+        
+        
         WindowGroup {
         
             NavigationView {
                 switch sessionService.state {
                 case .loggedIn:
                     
-                    if signUp.isMedicationSetup {
-                        CaretakerView()
-                            .environmentObject(sessionService)
-                    } else {
-                        MedicationSetup()
-                            .environmentObject(sessionService)
-                    }
-                   
+                    CaretakerView()
+                        .environmentObject(sessionService)
+                    
                 case .loggedOut:
+                    
+//                    if needsAppOnboarding {
+//                        OnboardingContainerView()
+//                    } else {
+//                        LoginView()
+//                            .environmentObject(sessionService)
+//                    }
+                    
                     LoginView()
                         .environmentObject(sessionService)
-                    //CaretakerView()
                 }
+
+            }
+            .onAppear() {
+                //Check if notification permissions have been asked
+                currentNotification.getNotificationSettings(completionHandler: { settings in
+                    //if not yet asked, ask and if yes set notification schedule
+                    if settings.authorizationStatus == .notDetermined {
+                        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+                            if success {
+                                NotificationService.shared.scheduleNotifications()
+                                notificationScheduleSet = true
+                            } else if let error = error {
+                                print(error.localizedDescription)
+                            }
+                        }
+                        //if already asked check if authorized
+                    } else if settings.authorizationStatus == .authorized {
+                        //Notification permision already granted set notification schedule if not yet set
+                        if !notificationScheduleSet  {
+                            NotificationService.shared.scheduleNotifications()
+                            notificationScheduleSet = true
+                        }
+                    }
+                })
+                
+                
+                
             }
             
         }
